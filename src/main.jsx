4e86ccd8a7306c7405a2060422a713ff93bd3db3
CTSETS <admin@ctsetsjm.com>
2026-03-28 12:30:25 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CTSApp from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CTSApp />
-  </React.StrictMode>
-);
